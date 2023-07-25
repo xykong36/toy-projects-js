2a7ac168187737ec96caf12ef4952cc060a088fc
XIANGYU KONG <xiangyukongdeveloper@gmail.com>
2023-07-24 20:45:24 -0400

    add the observer for upName

--- bb-2/scripts/content.js
+++ bb-2/scripts/content.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import { GoChevronDown, GoChevronUp } from "react-icons/go";
 
 
-const readingTime = 1000;
+const loadingTime = 1000;
 
 const Uplist = React.memo(({ allItems, show }) => {
   if (!show) {
@@ -54,33 +54,10 @@ const NoResultHint = React.memo(({ onClose }) => {
   );
 });
 
-setTimeout(() => {
-  // Send a message to the background script
-  let upName = document.querySelectorAll('.up-name')[0].text.trim()
-  console.log('upName:', upName);
-  chrome.runtime.sendMessage({ action: 'queryData', keyword: upName }, function (response) {
-    console.log('查询到的数据：', response);
-    let allItems = response;
-
-    // 创建一个新的 div 元素，作为 React 组件的容器
-    const reactContainer = document.createElement('div');
-
-    // 渲染 React 组件到新的 div 元素中
-    ReactDOM.render(
-      <App allItems={allItems} />,
-      reactContainer
-    );
-
-    // Support for API reference docs
-    const upInfoContainer = document.querySelector('.up-info-container');
-    upInfoContainer.insertAdjacentElement('afterend', reactContainer);
-  });
-}, readingTime);
-
-function App({ allItems }) {
+function App({ allItems, reset }) {
   const [showListContent, setShowListContent] = useState(false);
   const [showHint, setShowHint] = useState(false);
-  const [showToggleButton, setShowToggleButton] = useState(false); // 新增状态，用于控制 ToggleButton 的显示
+  const [showToggleButton, setShowToggleButton] = useState(false);
 
   const toggleListContent = useCallback(() => {
     setShowListContent(show => !show);
@@ -91,19 +68,70 @@ function App({ allItems }) {
   }, []);
 
   useEffect(() => {
+    setShowHint(false);
+    setShowToggleButton(false);
+  }, [reset]);
+
+  useEffect(() => {
     console.log('allItems:', allItems);
     if (allItems === '没有找到结果') {
       setShowHint(true);
     } else if (allItems.length > 0) {
-      setShowToggleButton(true); // 当有数据时，显示 ToggleButton
+      setShowToggleButton(true);
     }
   }, [allItems]);
 
   return (
     <>
-      {showToggleButton && <ToggleButton onClick={toggleListContent} showListContent={showListContent} />} {/* 使用 showToggleButton 控制 ToggleButton 的显示 */}
+      {showToggleButton && <ToggleButton onClick={toggleListContent} showListContent={showListContent} />}
       {showHint && <NoResultHint onClose={closeHint} />}
       <Uplist allItems={allItems} show={showListContent} />
     </>
   );
+}
+
+// 创建一个新的 div 元素，作为 React 组件的容器
+const reactContainer = document.createElement('div');
+
+// 定义一个函数来获取数据并渲染组件
+function fetchDataAndRender() {
+  let upNameDiv = document.querySelector('.up-name');
+  if (upNameDiv) {
+    let upName = upNameDiv.textContent.trim();
+    console.log('upName:', upName);
+    chrome.runtime.sendMessage({ action: 'queryData', keyword: upName }, function (response) {
+      console.log('查询到的数据：', response);
+      let allItems = response;
+
+      // 使用 setTimeout 确保 allItems 已经更新
+      setTimeout(() => {
+        // 渲染 React 组件到新的 div 元素中
+        ReactDOM.render(
+          <App allItems={allItems} reset={Date.now()} />,
+          reactContainer
+        );
+        const upInfoContainer = document.querySelector('.up-info-container');
+        upInfoContainer.insertAdjacentElement('afterend', reactContainer);
+      }, 0);
+    });
+  }
+}
+
+// 在页面加载时立即执行一次数据获取和组件渲染
+setTimeout(fetchDataAndRender, loadingTime);
+
+// 创建一个 MutationObserver 实例来监听 upName 的变化
+const observer = new MutationObserver((mutationsList, observer) => {
+  for (let mutation of mutationsList) {
+    if (mutation.type === 'childList' || mutation.type === 'characterData') {
+      // 在.up-name元素发生变化时重新获取数据并渲染组件
+      fetchDataAndRender();
+    }
+  }
+});
+
+// 开始监听 upName 的变化
+let upNameDiv = document.querySelector('.up-name');
+if (upNameDiv) {
+  observer.observe(upNameDiv, { childList: true, characterData: true, subtree: true });
 }
